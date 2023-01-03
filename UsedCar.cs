6feb22859c8beb2e42cd473bcda922d62d8b42ea
NazarMykolenko <100047241+NazarMykolenko@users.noreply.github.com>
2023-01-03 22:13:53 +0200

    Add files via upload

---
+++ UsedCar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClassLibrary
+{
+    public class UsedCar : Car    {
+        public decimal TraveledKm { get; set; }
+
+        public UsedCar(string make, string model, decimal price, int year, string color, decimal traveledKm) : base(make, model, price, year, color)
+        {
+            TraveledKm = traveledKm;
+        }
+
+        override public  string ToString()
+        {
+            return "Make: " + Make + " Model: " + Model + " Year: " + Year + " Color: " + Color + " Price: $" + Price + " Traveled: "+ TraveledKm+"km";
+        }
+    }
+}
